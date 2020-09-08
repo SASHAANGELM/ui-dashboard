f153077adae33b39fadb58df187ff2140288ea50
SASHAANGELM <SASHAANGELMwot@gmail.com>
2020-09-08 23:28:00 +0300

    Added layout for main content

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -8,7 +8,28 @@
         <div class="text-3xl font-bold">
           Hello! Leon
         </div>
-        <div class="text-sm text-gray-700">Never put off till tomorrow what can be done today!</div>
+        <div class="mb-8 text-sm text-gray-700">Never put off till tomorrow what can be done today!</div>
+
+        <div class="flex space-x-16">
+          <div class="w-4/6">
+            <div class="mb-6 text-gray-600 text-lg font-medium">Today</div>
+
+          </div>
+          <div class="w-2/6 ">
+            <div class="mb-6 text-gray-600 text-lg font-medium">Automatically assigned</div>
+            <div class="h-40 bg-gray-900 rounded-xl"></div>
+          </div>
+        </div>
+
+        <div class="flex space-x-16">
+          <div class="w-2/6 ">
+            <div class="mb-6 text-gray-600 text-lg font-medium">Mocup</div>
+            <div class="h-40 bg-gray-900 rounded-xl"></div>
+          </div>
+          <div class="w-4/6">
+            <div class="mb-6 text-gray-600 text-lg font-medium">Company</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
