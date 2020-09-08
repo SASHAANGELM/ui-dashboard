6e76963fba3d9b61a40f8b464b33ec151380dc87
SASHAANGELM <SASHAANGELMwot@gmail.com>
2020-09-08 23:15:33 +0300

    Added Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-end items-center h-16 mt-4">
+
+    <div class="relative mr-40">
+      <input type="text" class="w-64 h-10 pl-10 border border-transparent hover:border-gray-500 focus:border-indigo-500 focus:outline-none rounded-full text-sm" placeholder="Search">
+      <div class="absolute left-0 top-0 mt-2 ml-4">
+        <i class="fas fa-search"></i>
+      </div>
+    </div>
+
+    <button class="w-10 h-10 mr-8 hover:bg-white rounded-full text-gray-500 hover:text-black">
+      <i class="fas fa-cog"></i>
+    </button>
+    <button class="w-10 h-10 rounded-full overflow-hidden">
+      <img src="https://picsum.photos/40/40" alt="">
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+};
+</script>
+
+<style>
+
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="min-h-screen bg-gray-100">
+  <div class="min-h-screen bg-gray-200">
     <div class="flex min-h-screen">
       <Sidebar></Sidebar>
-      <div class="">main</div>
+      <div class="flex-1 pl-10 pr-16 pb-10">
+        <Header></Header>
+
+        <div class="text-3xl font-bold">
+          Hello! Leon
+        </div>
+        <div class="text-sm text-gray-700">Never put off till tomorrow what can be done today!</div>
+      </div>
     </div>
   </div>
 
@@ -10,10 +17,12 @@
 
 <script>
 import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
 
 export default {
   components: {
-    Sidebar
+    Sidebar,
+    Header
   }
 };
 
