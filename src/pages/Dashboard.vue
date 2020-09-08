f0bcf468d1686898fd5774ce6180f77bba582fab
Maxim <Crossfade143@gmail.com>
2020-09-08 21:20:39 +0300

    Created dashboard page

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="">
+      <div class="menu">TBK</div>
+      <div class="">Workbench</div>
+      <div class="">filters</div>
+      <div class="">Management</div>
+      <div class="">Unlock</div>
+      <div class="">Map</div>
+      <div class="">Search</div>
+    </div>
+    <div class="header">
+      <div class="search-line">Search</div>
+      <div class="settings">settings</div>
+      <div class="logo">LOGO</div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
